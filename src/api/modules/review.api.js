fb9a523d50d792d456d56794b6e6fb0488daafb8
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-20 19:55:37 +0800

    feat: Create review api module

---
+++ src/api/modules/review.api.js
@@ -0,0 +1,47 @@
+import privateClient from "../client/private.client";
+
+const reviewEndPoints = {
+  add: "reviews",
+  remove: ({ reviewId }) => `reviews/:${reviewId}`,
+  getList: "reviews",
+};
+
+const reviewApi = {
+  add: async ({ mediaId, mediaType, mediaTitle, mediaPoster, content }) => {
+    try {
+      const response = await privateClient.post(reviewEndPoints.add, {
+        mediaId,
+        mediaType,
+        mediaTitle,
+        mediaPoster,
+        content,
+      });
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+  remove: async ({ reviewId }) => {
+    try {
+      const response = await privateClient.post(
+        reviewEndPoints.remove({ reviewId })
+      );
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+  getList: async () => {
+    try {
+      const response = await privateClient.get(reviewEndPoints.getList);
+
+      return { response };
+    } catch (error) {
+      return { error };
+    }
+  },
+};
+
+export default reviewApi;
