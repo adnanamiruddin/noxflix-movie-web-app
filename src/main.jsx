7faf2807aa366ed6ef3886619b30f7e732fcaf4a
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-21 00:42:14 +0800

    feat: Add Provider from react-redux in main.jsx and use store.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-import "@fontsource/roboto/300.css"
-import "@fontsource/roboto/400.css"
-import "@fontsource/roboto/500.css"
-import "@fontsource/roboto/700.css"
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
+  // </React.StrictMode>
 );
