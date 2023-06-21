ee4ada10b34a16ff68efc37cd0cf174e18c1bfc1
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-21 19:27:45 +0800

    feat: Implements all redux features slice to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./features/userSlice";
 import themeModeSlice from "./features/themeModeSlice";
+import authModalSlice from "./features/authModalSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+import appStateSlice from "./features/appStateSlice";
 
 const store = configureStore({
   reducer: {
+    appState: appStateSlice,
     user: userSlice,
-    themeMode: themeModeSlice
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
   },
 });
 
