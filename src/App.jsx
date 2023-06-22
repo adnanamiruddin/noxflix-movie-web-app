7a51448a9158289b8e18f0420d36a1ec37c03474
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-23 02:02:50 +0800

    feat: Add react-router-dom to App.jsx, use routes.jsx from folder router and component PageWrapper

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { useSelector } from "react-redux";
 import themeConfigs from "./configs/theme.configs";
 import { ToastContainer } from "react-toastify";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layouts/MainLayout";
+import routes from "./routes/routes";
+import PageWrapper from "./components/common/PageWrapper";
 
 const App = () => {
   const { themeMode } = useSelector((state) => state.themeMode);
@@ -24,6 +28,46 @@ const App = () => {
       {/* Material UI reset CSS START */}
       <CssBaseline />
       {/* Material UI reset CSS END */}
+
+      {/* App Routest START */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {routes.map((route, i) =>
+              route.index ? (
+                <Route
+                  index
+                  key={i}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              ) : (
+                <Route
+                  key={i}
+                  path={route.path}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* App Routest END */}
     </ThemeProvider>
   );
 };
